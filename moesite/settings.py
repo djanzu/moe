ee31fb197f712284cf8d3316f300ac2cb9a87341
anzu takahashi <anzu@happy-girl.jp>
2021-11-07 14:22:27 +0900

    get app key from env

--- moesite/settings.py
+++ moesite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mmacxb0=!1h2o34d&2_uae2#38*!uxcovesu#=63=e9_tt4n7-'
+SECRET_KEY = os.environ.get('APP_SECRET', 'django-insecure-mmacxb0=!1h2o34d&2_uae2#38*!uxcovesu#=63=e9_tt4n7-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
