5f020991d1bf7bdf387ed555bc22b696ab40dc2d
anzu takahashi <anzu@happy-girl.jp>
2021-11-08 17:34:35 +0900

    見た目とか差分とか

--- moe/views.py
+++ moe/views.py
@@ -11,7 +11,26 @@ class KirokuView(generic.ListView):
     paginate_by = 25
 
     def get_context_data(self, **kwargs):
-        return super().get_context_data(**kwargs)
+        context =  super().get_context_data(**kwargs)
+        kiroku = models.Kiroku.objects.all().order_by('dt')
+        old_moyori = 0
+        old_moe = 0
+        out = []
+        for i in kiroku:
+            diff_moyori = 0 if old_moyori == 0 else int(i.moyori_cnt) - old_moyori
+            diff_moe = 0 if old_moe == 0 else int(i.moe_cnt) - old_moe
+            old_moyori = i.moyori_cnt
+            old_moe = i.moe_cnt
+            out.append({
+                "dt": i.dt,
+                "moyori_cnt": int(i.moyori_cnt),
+                "moe_cnt": int(i.moe_cnt),
+                "diff_moyori": diff_moyori,
+                "diff_moe": diff_moe,
+            })
+        context['datas'] = out
+            
+        return context
 
     def get_queryset(self):
         return models.Kiroku.objects.all().order_by('-dt')  
