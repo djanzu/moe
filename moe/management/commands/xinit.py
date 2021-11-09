033972f0b3c40ef44e1d20dd67f8f93bd0c61fb3
anzu takahashi <anzu@happy-girl.jp>
2021-11-09 11:30:02 +0900

    せっかくなので

--- moe/management/commands/xinit.py
+++ moe/management/commands/xinit.py
@@ -16,6 +16,7 @@ class Command(BaseCommand):
             {"dt": "2021/11/05 20:41", "moyori": 29662, "moe": 10453},
             {"dt": "2021/11/06 21:06", "moyori": 29726, "moe": 10521},
             {"dt": "2021/11/07 19:19", "moyori": 29777, "moe": 10572},
+            {"dt": "2021/11/08 20:30", "moyori": 29812, "moe": 10628},
         ]
         for i in x:
             dt = datetime.datetime.strptime(i['dt'], '%Y/%m/%d %H:%M')
